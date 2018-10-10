63a135d0c58263f067ae2c99ba9b4802c1bde02b
Frankie Delos Santos <frankie@swuser-129-8-202-250.swlan.csufresno.edu>
2018-10-10 09:05:29 -0700

    Lab4

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+interface IStudent {
+  id: number;
+  firstName: string;
+  lastName: string;
+  course: string;
+}
 
 @Component({
   selector: 'main-content',
@@ -7,9 +13,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContentComponent implements OnInit {
 
-  constructor() { }
+
+  student: Array<IStudent> = [];
+
+  editMode: boolean = false;
+  constructor() {
+    this.student[0] = {
+      id: 1,
+      firstName: "Franklin",
+      lastName: "Delos Santos",
+      course: "Programming"
+    }
+    this.student[1] = {
+      id: 1,
+      firstName: "Tom",
+      lastName: "Brady",
+      course: "Swimming"
+    }
+    this.student[2] = {
+      id: 1,
+      firstName: "Mark",
+      lastName: "Zuc",
+      course: "Business"
+    }
+    this.student[3] = {
+      id: 1,
+      firstName: "Joe",
+      lastName: "Biden",
+      course: "Politics"
+    }
+  }
 
   ngOnInit() {
   }
 
+  addStudent() {
+    this.editMode=true;
+    const student: IStudent = {
+      id: null,
+      firstName: null,
+      lastName: null,
+      course: null
+    };
+    // this.student.push(student);
+    this.student.unshift(student);
+  }
+
+  removeStudent(index: number){
+    console.log(`from removeStudent function index:, ${index}`);
+    this.student.splice(index, 1);
+
+  }
+
+  saveStudent(){
+    this.editMode = false;
+  }
 }
